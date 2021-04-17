f722c4e8da98b4e2bdce2579c5cdd4721d87ba53
Linux User <bubbyroosh@alpine.Home>
2021-04-17 15:00:47 -0500

    update

--- config.h
+++ config.h
@@ -62,7 +62,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char *scrotcmd[]  = { "scrot", NULL };
 static const char *volup[]  = { "amixer", "sset", "Master", "5+", NULL };
 static const char *voldown[]  = { "amixer", "sset", "Master", "5-", NULL };
